e0db83b813dddd6cd44cb15de6ce6ea5a6f76a84
Dyego Severo <dyego@efkz.com.br>
2026-06-25 13:42:16 -0300

    fix: workspace sem AppLayout (remove sidebar de clínica do WS)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation, Outlet } from "react-router-dom";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -247,7 +247,7 @@ function AnimatedRoutes() {
                 <Route
                   element={
                     <ProtectedRoute>
-                      <AppLayout />
+                      <Outlet />
                     </ProtectedRoute>
                   }
                 >
